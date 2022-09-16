028545da6a60b7c64ac53f0ea77e7f0b86f4edee
Jonathan Allen <Jonathan.Allen@snow.edu>
2022-09-16 14:32:23 -0600

    will it build?

--- src/Components/BasicGrid.tsx
+++ src/Components/BasicGrid.tsx
@@ -5,14 +5,6 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 import MediaCard from './MediaCard';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
 export default function BasicGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
